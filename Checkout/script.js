7a215e55a521f8235d192eb0e48c589a14046611
Egion Xerxa <egionxerxa@gmail.com>
2025-01-08 14:49:41 +0100

    Delete Checkout directory

--- Checkout/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('checkoutForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    document.getElementById('cardName').value = '';
-    document.getElementById('cardNumber').value = '';
-    document.getElementById('expiryDate').value = '';
-    document.getElementById('cvv').value = '';
-    document.getElementById('confirmationMessage').classList.remove('hidden');
-    document.getElementById('checkoutForm').classList.add('hidden');
-});
