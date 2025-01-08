0242610c2f2dfa0e94daa258f792631dda28b0df
Egion Xerxa <egionxerxa@gmail.com>
2025-01-08 14:49:23 +0100

    Delete Login and Register directory

--- Login and Register/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll("a").forEach(anchor => {
-    anchor.addEventListener("click", e => {
-        e.preventDefault();
-        alert("This feature is not implemented.");
-    });
-});
