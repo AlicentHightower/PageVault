6fd92d9dd5d059a132225c0279fb08c999eabf76
Egion Xerxa <egionxerxa@gmail.com>
2025-02-05 11:59:31 +0100

    Delete PageVault directory

--- PageVault/PageVault/Contact us/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
-    document.getElementById('confirmationMessage').classList.remove('hidden');
-    document.getElementById('contactForm').classList.add('hidden');
-});
--- PageVault/PageVault/Home/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.book').forEach(book => {
-    book.addEventListener('click', () => {
-        alert(`More details about "${book.querySelector('h3').textContent}" coming soon!`);
-    });
-});
